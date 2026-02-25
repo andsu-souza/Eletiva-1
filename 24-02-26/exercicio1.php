ad6155f59ca44fa1f20030269c2f9b3222f8ca5c
andsu-souza <and.jbr.souza@gmail.com>
2026-02-24 21:27:33 -0300

    Configuração e começo de exercicios

--- 24-02-26/exercicio1.php
+++ 24-02-26/exercicio1.php
@@ -4,27 +4,24 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Primeiro Exemplo PHP</title>
+    <title>Exercício 1</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
     <div class="container py-3">
-        <h1>Primeiro Exemplo PHP</h1>
-        <h3>Data de hoje:<?php echo date("d/m/Y"); ?> </h3>
-        <form method="post" action="exemplo.php">
+        <h1>Exercício 1</h1>
+        <form method="post">
             <div class="mb-3">
-                <label for="Nome" class="form-label">Informe seu nome:</label>
-                <input type="text" id="Nome" name="Nome" class="form-control" required="">
+                <label for="valor1" class="form-label">Informe o primeiro valor:</label>
+                <input type="number" id="valor1" name="valor1" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+                <label for="valor2" class="form-label">Informe o segundo valor:</label>
+                <input type="number" id="valor2" name="valor2" class="form-control" required="">
             </div>
             <button type="submit" class="btn btn-primary">Enviar</button>
         </form>
-        <?php
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $nome = $_POST['Nome'];
-            echo "<p>Bem Vindo(a) $nome!</p>";
-        }
-        ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
     </div>
 </body>
--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Código-fonte produzido na disciplina de Eletiva I - Linguagem de Programação 
 https://github.com/vanessaborges123/EletivaI-2026-1/
 
 Ver vídeos sobre github do professor guanabara
+
+vanessaborges2.github.io/Gerador-Formulario
+
+Gerador de Formulário
+
+php -S localhost:8000
