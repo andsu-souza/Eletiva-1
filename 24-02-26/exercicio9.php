a0141b3620661d17bbcf6ae0e901f41ad109828f
andsu-souza <and.jbr.souza@gmail.com>
2026-03-03 19:21:47 -0300

    exercicios 10 e 11

--- 24-02-26/exercicio9.php
+++ 24-02-26/exercicio9.php
@@ -21,7 +21,7 @@
         <?php
             if($_SERVER['REQUEST_METHOD'] == "POST")
                     $valor1 = $_POST['valor1'];
-                    $calcula = $valor1 * 3.14 * 2;
+                    $calcula = ($valor1 * $valor1) * 3.14;
                     echo "A área do circulo é: $calcula";
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
