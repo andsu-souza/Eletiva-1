7d6a9962023d259a22b786c8c2e2c1967f2d8dcc
andsu-souza <and.jbr.souza@gmail.com>
2026-03-10 20:37:45 -0300

    ex 9 list 3

--- Aula 03-03/exercicio9.php
+++ Aula 03-03/exercicio9.php
@@ -4,13 +4,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercício 8</title>
+    <title>Exercício 9</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
     <div class="container py-3">
-        <h1>Exercício 8</h1>
+        <h1>Exercício 9</h1>
         <form method="post">
             <div class="mb-3">
                 <label for="valor1" class="form-label">Digite o número:</label>
@@ -22,10 +22,11 @@
             if($_SERVER['REQUEST_METHOD'] == "POST"){
                 $valor1 = $_POST['valor1'];
             
-                do {
-                    echo "<p>$valor1";
-                    $valor1 = $valor1 - 1;
-                } while($valor1 >= 1);
+                $fatorial = 1;
+                for($i = 1; $i <= $valor1; $i++){
+                    $fatorial = $fatorial * $i;
+                }
+                echo "<p>$fatorial</p>";
             }
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
