904b1b692db68138ff6443ce39503f3f58611985
andsu-souza <and.jbr.souza@gmail.com>
2026-03-10 21:49:56 -0300

    ex 5 list 4

---
+++ Aula 10-03/exercicio5.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Exercício 5</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+  <div class="container py-3">
+    <h1>Exercício 5</h1>
+    <form method="post">
+      <div class="mb-3">
+        <label for="word1" class="form-label">Digite o número:</label>
+        <input type="text" id="word1" name="word1" class="form-control" required="">
+      </div>
+      <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+    <?php
+            if($_SERVER['REQUEST_METHOD'] == "POST"){
+                $word1 = $_POST['word1'];
+
+                $numero = sqrt($word1);
+                echo "<p>A raiz de $word1 é $numero!</p>";
+            }
+        ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
+      crossorigin="anonymous"></script>
+  </div>
+</body>
+
+</html>
