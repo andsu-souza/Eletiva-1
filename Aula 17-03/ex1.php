b0508977a2207477a19be675c988ecab92886731
andsu-souza <and.jbr.souza@gmail.com>
2026-03-17 20:12:02 -0300

    exemplo aula 17-03

--- Aula 17-03/ex1.php
+++ Aula 17-03/ex1.php
@@ -12,12 +12,28 @@
   <div class="container py-3">
     <h1>Exercício 1</h1>
     <form method="post">
-      <div class="mb-3">
+    <?php  
+    for($i = 0; $i<7;$i++)
+      echo '
+    <div class="mb-3">
         <label for="nota[]" class="form-label">Informa a nota: </label>
         <input type="number" id="nota[]" name="nota[]" class="form-control" required="">
-      </div>
+      </div>'
+      ?>
       <button type="submit" class="btn btn-primary">Enviar</button>
     </form>
+    <?php
+      if($_SERVER['REQUEST_METHOD'] == "POST"){
+        $mapa = $_POST['nota'];
+        $copia_mapa = $mapa;
+        var_dump($mapa);
+        sort($mapa);
+        echo "<p>O menor valor é: ".$mapa[0]."</P>";
+        $posicao = array_search($mapa[0], $copia_mapa);
+        echo "<p>Na posição $posicao </p>";
+
+      }
+    ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
       crossorigin="anonymous"></script>
